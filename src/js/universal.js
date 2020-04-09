932ebf29c29e7d460062ac40ea4711b05e1ca3ab
crissyw <widjaja.crystal@gmail.com>
2020-04-09 11:07:50 -0700

    added universal header for include

---
+++ src/js/universal.js
@@ -0,0 +1,8 @@
+// Automation to include all js files
+$(function(){
+var includes = $('[data-include]');
+jQuery.each(includes, function(){
+    var file = '/' + $(this).data('include') + '.html';
+    $(this).load(file);
+});
+});
